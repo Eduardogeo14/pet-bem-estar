3d039e8704dc046db4721508b5ad5a78182bf989
Eduardo Ibargoyen <dudugeo2014@gmail.com>
2026-08-18 13:16:33 -0300

    feat: atualiza classe Pet com relacionamentos

--- src/main/java/br/edu/ifrs/petbemestar/dominio/Pet.java
+++ src/main/java/br/edu/ifrs/petbemestar/dominio/Pet.java
@@ -1,5 +1,111 @@
 package br.edu.ifrs.petbemestar.dominio;
 
-public class Pet {
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+public class Pet {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nomeBicho;
+	private String raca;
+	private int idade;
+	private String observacoes;
+	
+	@Enumerated(EnumType.STRING)
+	private TipoBichoEnum tipoBicho;
+	
+	
+	private Cliente cliente;
+	
+	private List<Atendimento> atendimentos = new ArrayList<>();
+	
+	public Pet() {}
+	
+	public Pet(TipoBichoEnum tipoBicho, String nomeBicho, String raca, int idade, String observacoes) {
+		setTipoBicho(tipoBicho);
+		setNomeBicho(nomeBicho);
+		this.raca = raca;
+		setIdade(idade);
+		this.observacoes = observacoes;
+	}
+	
+	
+	public void adicionarAtendimento(Atendimento atendimento) {
+		this.atendimentos.add(atendimento);
+		atendimento.setPet(this);
+	}
+
+	public void setNomeBicho(String nomeBicho) {
+		if(nomeBicho == null || nomeBicho.trim().isEmpty()) {
+			throw new IllegalArgumentException("Nome inválido");
+		} else {
+			this.nomeBicho = nomeBicho;
+		}
+	}
+	
+	public void setTipoBicho(TipoBichoEnum tipoBicho) {
+		if(tipoBicho == null) {
+			throw new IllegalArgumentException("Tipo de bicho inválido");
+		} else {
+			this.tipoBicho = tipoBicho;
+		}
+	}
+	
+	public void setIdade(int idade) {
+		if(idade < 0) {
+			throw new IllegalArgumentException("Idade inválida");
+		} else {
+			this.idade = idade;
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeBicho() {
+		return this.nomeBicho;
+	}
+	
+	public String getRaca() {
+		return this.raca;
+	}
+	
+	public TipoBichoEnum getTipoBicho() {
+		return this.tipoBicho;
+	}
+
+	public int getIdade() {
+		return this.idade;
+	}
+	
+	public String getObservacoes() {
+		return this.observacoes;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<Atendimento> getAtendimentos() {
+		return atendimentos;
+	}
 }
