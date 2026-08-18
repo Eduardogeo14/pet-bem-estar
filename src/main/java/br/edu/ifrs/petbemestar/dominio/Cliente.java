544f74db0bb8f2035dc767ad0288e81f1c226553
Eduardo Ibargoyen <dudugeo2014@gmail.com>
2026-08-18 12:54:53 -0300

    Adiciona classe de dominio Cliente

---
+++ src/main/java/br/edu/ifrs/petbemestar/dominio/Cliente.java
@@ -0,0 +1,12 @@
+package br.edu.ifrs.petbemestar.dominio;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cliente {
+
+	
+}
